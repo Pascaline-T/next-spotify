73e845d0d1637ca54fcceac3b79e71e760f4025b
GrosboillotFlo <grosboillotflo@aol.com>
2021-05-27 16:28:39 +0200

    Merging back and front

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,22 +8,42 @@ type Props = {
 
 const NavBar: React.FC<Props> = ({ isLoggedIn, spotifyLoginUrl }) => {
   return (
-    <nav>
-      <nav className="sidebar"></nav>
-      <div className="header"></div>
-      <main></main>
-      {/* {isLoggedIn ? (
-        <>
-          <p>
+    <div className="dropdown text-secondary">
+      <a
+        className="nav-link dropdown-toggle"
+        id="navbarDropdownMenuLink"
+        role="button"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="bi bi-person"
+          viewBox="0 0 16 16"
+        >
+          <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+        </svg>
+      </a>
+      <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+        {isLoggedIn ? (
+          <li>
             <a href="/api/logout">logout</a>
-          </p>
-        </>
-      ) : (
-        <p>
-          <a href={spotifyLoginUrl}>login</a>
-        </p>
-      )} */}
-    </nav>
+          </li>
+        ) : (
+          <li>
+            <a href={spotifyLoginUrl}>login</a>
+          </li>
+        )}
+      </ul>
+      <style jsx>{`
+        div {
+          background-color: black;
+        }
+      `}</style>
+    </div>
   );
 };
 
@@ -36,7 +56,7 @@ export const Layout: React.FC<Props> = ({ children, isLoggedIn, spotifyLoginUrl 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"></link>
       </Head>
       <NavBar isLoggedIn={isLoggedIn} spotifyLoginUrl={spotifyLoginUrl} />
-      <main>{children}</main>
+      <main className="bg-light">{children}</main>
     </>
   );
 };
